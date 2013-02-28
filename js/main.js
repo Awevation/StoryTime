a92bf58c3db846ccdfb12269d51ea7d48d6caeee
Milo Mordaunt <milomord@gmail.com>
2013-02-28 15:47:22 +0000

    added basic form, now starts a pad of the users choice despite looking ugly

--- js/main.js
+++ js/main.js
@@ -4,17 +4,33 @@ $(document).ready(function() {
 
 function main() {
     $('#storyButton').click(function() {
-	var padDetails = storyForm();
-	genPad();
+	$(document).load('../storyForm.html', function(res, status, req) {
+	    if (status != "error") {
+		$('#pad').before(res);
+	    } else {
+		alert("Well would you look at that.... AN ERROR OCCURRED! Accept my sincerest apologyies.");
+	    }
+
+	    $('#submit').click(function() {
+		genPad(storyForm());
+		$('#form').remove();
+	    });
+	});
     });
+
 }
 
 function storyForm() {
+    function form() {
+	this.name = $('input[name=padName]').val();
+    }
+
+    return new form();
 }
 
 function genPad(padDetails) {
     $('#pad').pad({
-	'padId':'storytime',
+	'padId':padDetails.name,
 	'showChat':'true',
 	'showControls':'true',
 	'height':$('#pad').css("height") //for some reason the pad doesn't pay attention to the wrapper's height, only its width 
